00b6989323230610c3a3fcf8beb89fb217202e39
I_Agreed <48776053+I-Agreed@users.noreply.github.com>
2022-09-01 15:22:54 +1000

    usePeople fixed (temp - probably)

--- src/stores/usePeople.js
+++ src/stores/usePeople.js
@@ -7,23 +7,23 @@ import { User, Role } from 'src/scripts/objects.js'
 //      add perms for roles per archetype in the same subject
 //      add methods to add, remove, and edit items in an archetype
 
-
+const roles = [
+    new Role("Teacher", {}),
+    new Role("Student", {})
+]
 
 export const usePeople = defineStore('peopleStore', {
     state: () => {
         return {
-            roles: [
-                Role("Teacher", {}),
-                Role("Student", {})
-            ],
+            roles: roles,
             users: [
-                User(444444444, "Dat",       "Huynh",     roles[0]),
-                User(444444444, "Jennifer",  "May",       roles[0]),
-                User(444444444, "Rebecca",   "Dam",       roles[0]),
-                User(444444444, "Johnathan", "Min",       roles[1]),
-                User(444444444, "Brendan",   "Alcorn",    roles[1]),
-                User(444444444, "Kai",       "Cao",       roles[1]),
-                User(444444444, "Klim",      "Tsoutsman", roles[1])
+                new User(444444444, "Dat", "Huynh", roles[0]),
+                new User(444444444, "Jennifer", "May", roles[0]),
+                new User(444444444, "Rebecca", "Dam", roles[0]),
+                new User(444444444, "Johnathan", "Min", roles[1]),
+                new User(444444444, "Brendan", "Alcorn", roles[1]),
+                new User(444444444, "Kai", "Cao", roles[1]),
+                new User(444444444, "Klim", "Tsoutsman", roles[1])
             ]
         }
     }
