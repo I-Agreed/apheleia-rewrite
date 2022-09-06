3c8da4168d7a0911bba5c133b8c29c1d8ccd0c5b
I_Agreed <48776053+I-Agreed@users.noreply.github.com>
2022-09-07 09:13:17 +1000

    merge

--- src/pages/dashboard/Loans.vue
+++ src/pages/dashboard/Loans.vue
@@ -3,11 +3,6 @@
         <div class="q-pa-md" style="height: 85%;">
             <!-- Current Loans Title -->
             <h5>Current Loans</h5>
-<<<<<<< HEAD
-
-            <!-- Current Loans Table -->
-            <q-table :rows="selfSt.currentLoansRows()" :columns="selfSt.currentLoansColumns" row-key="name" :hide-pagination="true" :rows-per-page-options="[0]" style="height: 100%;" separator="cell"/>
-=======
             <!-- Load information from the store into a table -->
             <q-table row-key="name"
                      style="height: 100%;"
@@ -16,7 +11,6 @@
                     :columns="selfSt.currentLoansColumns"
                     :hide-pagination="true"
                     :rows-per-page-options="[0]" />
->>>>>>> 771e4f3d407b429780f9088388a1ceb7601bbe48
         </div>
     </div>
 </template>
--- src/pages/items/EditLend.vue
+++ src/pages/items/EditLend.vue
@@ -25,11 +25,7 @@
                 <q-input v-model="time" filled type="date" style="padding-top: 5px;"/>
             </q-card-section>
 
-<<<<<<< HEAD
-            <!-- Bottom buttons -->
-=======
             <!-- Exit buttons -->
->>>>>>> 771e4f3d407b429780f9088388a1ceb7601bbe48
             <q-card-actions align="right">
                 <q-btn flat label="Return Now" color="primary" v-close-popup @click="inventorySt.returnItem(itemsLocalSt.focused_item)"/>
                 <q-btn flat label="Cancel" color="primary" v-close-popup />
